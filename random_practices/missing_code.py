c2aa35edefcc5cbb33900a8862b6a49f02825b91
Samuel <sundaysamuel2117@gmail.com>
2022-10-06 11:53:47 +0100

    ..

---
+++ random_practices/missing_code.py
@@ -0,0 +1,8 @@
+grade = int(input("Enter the grade: "))
+
+if grade > 90:
+    print(f"Congratulations your grade of {grade} earns you an A in this course")
+
+o = int(input("Enter the hourly wage: "))
+p = int(input("Enter the percentage increase or decrease: "))
+n = int(input("Enter the number of years: "))
